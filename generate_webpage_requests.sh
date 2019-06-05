30e08159768521c8ab91272bed502ca38ac4102a
root <root@alexandra1.europe-west1-b.c.alexandra-234812.internal>
2019-06-05 18:31:25 +0000

    better version

--- generate_webpage_requests.sh
+++ generate_webpage_requests.sh
@@ -5,7 +5,7 @@ MAX_NO_SUCCESS_REQUESTS="10"
 MAX_NO_ERROR_REQUESTS="5"
 HOST="localhost"
 PORT="8080"
-CHECKED_PAGE="apps-shared_dir-apache/index.php"
+CHECKED_PAGE="/index2.php"
 
 
 while true 
@@ -29,12 +29,14 @@ STATUS=$(cat RESPONSE_TO_REQUEST | grep "HTTP request sent" | awk -F "response" 
 		for s_request in $(seq 1 ${NO_SUCCESS_REQUESTS})
 		do
 			wget --spider -o SUCCESS_REQUEST  http://${HOST}:${PORT}/${CHECKED_PAGE} 
+			sudo docker exec -i zabbix-server zabbix_sender -vv -z localhost -s "Zabbix server" -p 10051 -k "apache_request_200" -o ${NO_SUCCES_REQUESTS}
 		done;
 
 		NO_ERROR_REQUESTS=$((1 + RANDOM % ${MAX_NO_ERROR_REQUESTS}))
 		for e_requests in $(seq 1 ${NO_ERROR_REQUESTS})
 		do
        			wget --spider -o ERROR_REQUEST  http://${HOST}:${PORT}/${CHECKED_PAGE}_INEXISTANT_WEBPAGE	
+			sudo docker exec -i zabbix-server zabbix_sender -vv -z localhost -s "Zabbix server" -p 10051 -k "apache_request_error" -o ${NO_ERROR_REQUESTS}
 		done;
 
    fi
